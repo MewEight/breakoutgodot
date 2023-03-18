9ffd66e3bc75d0d9b80aa005777846b443ddef1d
meweight <saychong1991@hotmail.com>
2023-03-18 22:01:04 +0100

    - Added some tweens

--- Scripts/Brick.gd
+++ Scripts/Brick.gd
@@ -5,5 +5,7 @@ func SetSprite(rect):
 	pass
 
 func DestroyBrick():
-	queue_free()
+	$BrickArea.queue_free()
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(0,0), 0.35).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
 	pass
